a91ea1da84e6ffc306724d118aeff897984d6ffe
gabeshaughnessy <gabeshaughnessy@gmail.com>
2015-06-26 16:07:17 -0700

    social updates

--- wp-content/themes/envision2015/README.md
+++ wp-content/themes/envision2015/README.md
@@ -2,12 +2,18 @@
 This is where the magic happens. Activate this theme for a cool updated site.
 
 ## To do:
- - [ ] supplier post grid
- - [ ] supplier filtering by type
- - [ ] project tags
- - [ ] project filter by type
- - [ ] get houzz and pinterest profiles
+ - [x] supplier post grid
+ - [x] supplier filtering by type
+ - [x] project tags
+ - [x] project filter by type
+ - [ ] get houzz and pinterest profiles - comment out houzz until their profile is ready.
+ - [ ] remove house logo temporarily
  - [ ] about page content
  - [ ] wp seo plugin and page titles
  - [ ] add google analytics and tracking events
  - [ ] new version of the logo 
+ - [ ] Constrain the main content area to the outer container
+ - [ ] About Page content - people, projects and showroom - get content from the doc.
+ - [ ] Migrate media from local to the staging site.
+ - [ ] get ai logo from marianne.
+ - [ ] cross browser support 
--- wp-content/themes/envision2015/template-parts/social-profile-banner.php
+++ wp-content/themes/envision2015/template-parts/social-profile-banner.php
@@ -1,6 +1,6 @@
 <?php global $post; ?>
 <div class="social-profile-banner">
-	<div class="profile-card">
+	<!-- <div class="profile-card">
 		<a href="http://houzz.com/envisioninteriors" title="" class="profile-icon">
 			<img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/social/houzz-logo.png" alt="profile icon alt" width="100%" height="auto" />
 		</a>
@@ -8,15 +8,15 @@
 			<h5>Find us On House</h5>
 			<span class="profile-url">houzz.com/envisioninteriors</span>
 		</a>
-	</div>			
+	</div> -->			
 	<div class="profile-card">
-		<a href="http://pinterest.com/envisioninteriors" title="find us on houzz" class="profile-icon pinterest">
+		<a href="http://pinterest.com/envisioninteriors" title="Envision Interiors on Pinterest" class="profile-icon pinterest">
 			<img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/social/pinterest-badge.png" alt="profile icon alt" width="100%" height="auto" />
 		</a>
 		
-		<a class="profile-details" href="http://pinterest.com/envisioninteriors" >
-			<h5>Follow us on Pinterest</h5>
-			<span class="profile-url">pinterest.com/envisioninteriors</span>
+		<a class="profile-details" href="https://www.pinterest.com/envisioninterio/" >
+			<h5>Find us on Pinterest</h5>
+			<span class="profile-url">View our Boards</span>
 		</a>
 	</div>
 
