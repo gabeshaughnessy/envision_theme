1837231b72ef057ccb3aec8e36641823a8275920
gabeshaughnessy <gabeshaughnessy@gmail.com>
2015-06-26 16:53:00 -0700

    page content

--- wp-content/themes/envision2015/template-parts/page-content.php
+++ wp-content/themes/envision2015/template-parts/page-content.php
@@ -2,10 +2,14 @@
 <article class="page-content">
 	<div class="outer container">
 		<?php 
-		if(have_posts()) : while(have_posts()) : the_post();
-		the_content(); 
-		endwhile;
-		endif;
+		if(!is_page_template('template-pages/page-home.php')){
+			if(have_posts()) : while(have_posts()) : the_post();
+			the_content(); 
+			endwhile;
+			endif;
+		} else{
+			the_content();
+		}
 		?>
 	</div>
 </article>
