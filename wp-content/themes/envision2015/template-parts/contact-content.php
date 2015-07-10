3d23a19124bd87d37871814bfb4cc97f98e41e88
gabeshaughnessy <gabeshaughnessy@gmail.com>
2015-07-09 17:55:12 -0700

    contact content

--- wp-content/themes/envision2015/template-parts/contact-content.php
+++ wp-content/themes/envision2015/template-parts/contact-content.php
@@ -1,7 +1,7 @@
 <?php global $post; ?>
 <div class="dark-blue-wrapper">
 	<div class="outer container">
-		<p class="lead">Paragraph about how Envision Interiors doesn’t sell to the public, but you would still be able to get them in touch with a contractor, designer, etc</p>
+		<?php get_template_part('template-parts/lead'); ?>
 		
 		<?php get_template_part('template-parts/contact-details'); ?>
 		<?php get_template_part('template-parts/map-and-form-banner'); ?>
